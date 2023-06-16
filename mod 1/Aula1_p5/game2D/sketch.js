9567511a075c47397eea2d48dd2a8788ebc077a1
Alves-py <zashamktr@gmail.com>
2023-06-16 13:03:39 -0300

    Game2D att

--- mod 1/Aula1_p5/game2D/sketch.js
+++ mod 1/Aula1_p5/game2D/sketch.js
@@ -1,9 +1,72 @@
-//Todo: Irei implementar posteriormente
+let personagem
+let personagemFinal
+let grama
+
+let tamanho = 64
+let eixoX = 0
+let eixoY = 0
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(576, 576);
+  personagem = loadImage('./imagens/kakashi.png')
+  personagemFinal = loadImage('./imagens/kakashiHokage.jpeg')
+  grama = loadImage('./imagens/grama.jpeg')
 }
 
 function draw() {
-  background(220);
+  if(eixoX < 0){
+    eixoX = 0
+  }
+  if(eixoX > tamanho*8){
+    eixoX = tamanho*8
+  }
+  if(eixoY < 0){
+    eixoY = 0
+  }
+  if(eixoY > tamanho*8){
+    eixoY = tamanho*8
+  }
+
+  background(255);
+
+  for(let i = 0; i <9; i++){
+    for(let j = 0; j <9; j++){
+      image(grama, i * tamanho, j * tamanho, tamanho, tamanho);
+    }
+
+  }
+
+  image(personagem, eixoX, eixoY, tamanho, tamanho);
+
+  if(eixoX === tamanho*8 && eixoY === tamanho*8){
+    image(personagemFinal, 200, 150, 150, 250)
+    textSize(35)
+    text('Parabéns! Kakashi virou \n           Hokage!', 100, 40)
+    
+    restart = createButton('Reiniciar')
+    restart.mousePressed(reset)
+    
+    noLoop()
+  }
+}
+function reset(){
+  eixoX = 0
+  eixoY = 0
+  restart.remove()
+
+  loop()
+}
+function keyPressed(){
+  if(keyIsDown(RIGHT_ARROW)){
+    eixoX += tamanho;
+  }
+  if(keyIsDown(DOWN_ARROW)){
+    eixoY += tamanho;
+  }
+  if(keyIsDown(LEFT_ARROW)){
+    eixoX -= tamanho;
+  }
+  if(keyIsDown(UP_ARROW)){
+    eixoY -= tamanho;
+  }
 }
