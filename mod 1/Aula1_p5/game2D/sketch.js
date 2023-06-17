146c2268f08926d82a92d71cef44a48f9a741da0
Alves-py <zashamktr@gmail.com>
2023-06-17 16:50:40 -0300

    Game2D finished

--- mod 1/Aula1_p5/game2D/sketch.js
+++ mod 1/Aula1_p5/game2D/sketch.js
@@ -8,16 +8,21 @@ let stratTime;
 let endTime;
 let duration;
 
+let movimento = 0;
 let tamanho = 64;
 let eixoX = 0;
 let eixoY = 0;
 
+let Paused = false;
+let pauseDuration = 5000;
+
 function setup() {
   createCanvas(576, 576);
   personagem = loadImage('./imagens/kakashi.png');
-  personagemFinal = loadImage('./imagens/kakashiHokage.jpeg');
+  personagemFinal = loadImage('./imagens/kakashiHokage.png');
   grama = loadImage('./imagens/grama.jpeg');
-
+  personagemPerdeu = loadImage('./imagens/kakashiGG.png')
+  persoTime = loadImage('./imagens/KakashiLendo.png')
   generateRandom();
   stratTime = millis();
 
@@ -47,15 +52,49 @@ function draw() {
 
   image(personagem, eixoX, eixoY, tamanho, tamanho);
 
+  if(Paused){
+    textSize(35);
+    textStyle(BOLDITALIC);
+    fill(255);
+    text('Cansou! Kakashi parou para ler\n               chia chia!', 20, 80)
+    textSize(25)
+    fill(255);
+    text('Espere por 5 segundos! ',120, 430 )
+    image(persoTime, 150, 150, 200, 200)
+  } else{
+    persoTime.remove
+  }
+
   if(eixoX === tamanho* randomX && eixoY === tamanho*randomY){
     endTime = millis();
     duration = (endTime - stratTime)/ 1000;
 
     image(personagemFinal, 200, 150, 150, 250);
     textSize(35);
+    fill(255);
     textStyle(BOLDITALIC);
     text('Parabéns! Kakashi virou \n           Hokage!', 100, 80)
     textSize(25)
+    fill(255);
+    text('Você levou: ' +duration.toFixed(2) + ' segundos',120, 450 )
+    
+    restart = createButton('Reiniciar');
+    restart.mousePressed(reset);
+    
+    noLoop();
+  }
+
+  if(movimento > 45){
+    endTime = millis();
+    duration = (endTime - stratTime)/ 1000;
+
+    image(personagemPerdeu, 150, 150, 250, 250);
+    textSize(35);
+    fill(255);
+    textStyle(BOLDITALIC);
+    text('Perdeu! Kakashi ficou sem \n           chakra!', 100, 80)
+    textSize(25)
+    fill(255);
     text('Você levou: ' +duration.toFixed(2) + ' segundos',120, 450 )
     
     restart = createButton('Reiniciar');
@@ -68,6 +107,7 @@ function draw() {
 function reset(){
   eixoX = 0;
   eixoY = 0;
+  movimento = 0;
   restart.remove();
   generateRandom();
   stratTime = millis();
@@ -79,16 +119,31 @@ function generateRandom(){
 }
 
 function keyPressed(){
-  if(keyIsDown(RIGHT_ARROW)){
-    eixoX += tamanho;
+  if(!Paused){
+    if(keyIsDown(RIGHT_ARROW)){
+      eixoX += tamanho;
+      movimento++;
+    }
+    if(keyIsDown(DOWN_ARROW)){
+      eixoY += tamanho;
+      movimento++;
+    }
+    if(keyIsDown(LEFT_ARROW)){
+      eixoX -= tamanho;
+      movimento++;
+    }
+    if(keyIsDown(UP_ARROW)){
+      eixoY -= tamanho;
+      movimento++;
+    }
+
+    if (movimento === 10 || movimento === 25){
+      Paused = true
+      setTimeout(resumeMovement, pauseDuration)
+    }
   }
-  if(keyIsDown(DOWN_ARROW)){
-    eixoY += tamanho;
-  }
-  if(keyIsDown(LEFT_ARROW)){
-    eixoX -= tamanho;
-  }
-  if(keyIsDown(UP_ARROW)){
-    eixoY -= tamanho;
-  }
+}
+
+function resumeMovement(){
+  Paused = false;
 }
