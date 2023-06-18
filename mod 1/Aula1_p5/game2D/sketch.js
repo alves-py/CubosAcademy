42b849772522353fac4f6361c2c35a0febe2a155
Alves-py <zashamktr@gmail.com>
2023-06-18 11:28:45 -0300

    Adding bestScore

--- mod 1/Aula1_p5/game2D/sketch.js
+++ mod 1/Aula1_p5/game2D/sketch.js
@@ -3,18 +3,19 @@ let personagemFinal;
 let grama;
 let randomX;
 let randomY;
-
 let stratTime;
 let endTime;
 let duration;
+let bestScore;
 
+let round = 0;
 let movimento = 0;
 let tamanho = 64;
 let eixoX = 0;
 let eixoY = 0;
-
-let Paused = false;
 let pauseDuration = 5000;
+let Paused = false;
+
 
 function setup() {
   createCanvas(576, 576);
@@ -51,7 +52,7 @@ function draw() {
   }
 
   image(personagem, eixoX, eixoY, tamanho, tamanho);
-
+  
   if(Paused){
     textSize(35);
     textStyle(BOLDITALIC);
@@ -68,6 +69,7 @@ function draw() {
   if(eixoX === tamanho* randomX && eixoY === tamanho*randomY){
     endTime = millis();
     duration = (endTime - stratTime)/ 1000;
+    score();
 
     image(personagemFinal, 200, 150, 150, 250);
     textSize(35);
@@ -77,17 +79,18 @@ function draw() {
     textSize(25)
     fill(255);
     text('Você levou: ' +duration.toFixed(2) + ' segundos',120, 450 )
+    fill(255);
+    text('Melhor tempo: ' +bestScore.toFixed(2)+' segundos', 100,480 )
     
     restart = createButton('Reiniciar');
     restart.mousePressed(reset);
-    
+  
     noLoop();
   }
 
   if(movimento > 45){
     endTime = millis();
     duration = (endTime - stratTime)/ 1000;
-
     image(personagemPerdeu, 150, 150, 250, 250);
     textSize(35);
     fill(255);
@@ -96,6 +99,7 @@ function draw() {
     textSize(25)
     fill(255);
     text('Você levou: ' +duration.toFixed(2) + ' segundos',120, 450 )
+
     
     restart = createButton('Reiniciar');
     restart.mousePressed(reset);
@@ -103,11 +107,21 @@ function draw() {
     noLoop();
   }
 }
+function score(){
+  if(round === 0){
+    bestScore = duration;
+  } else {
+    if (duration <= bestScore){
+      bestScore = duration;
+    }
+  }
+}
 
 function reset(){
   eixoX = 0;
   eixoY = 0;
   movimento = 0;
+  round++;
   restart.remove();
   generateRandom();
   stratTime = millis();
