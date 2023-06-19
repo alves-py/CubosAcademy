75f972fbd32b0555ee99d10a2ecb400f00cc4398
Alves-py <zashamktr@gmail.com>
2023-06-19 17:36:14 -0300

    Exercícios propostos na aula

---
+++ mod 1/Aula3_variaveis/exercicio2.js
@@ -0,0 +1,7 @@
+let populacaoCidade = 500_000;
+
+console.log(populacaoCidade);
+
+populacaoCidade = 1_000_000;
+
+console.log(populacaoCidade);
---
+++ mod 1/Aula3_variaveis/exercicio4.js
@@ -0,0 +1,15 @@
+let x = 10, y = 2;
+
+let resultado;
+
+resultado = x + y;
+console.log(`A soma de ${x} e ${y} é ${resultado}`);
+
+resultado = x - y;
+console.log(`A subtração de ${x} e ${y} é ${resultado}`);
+
+resultado = x * y;
+console.log(`A multiplicaçao de ${x} e ${y} é ${resultado}`);
+
+resultado = x / y;
+console.log(`A divisão de  ${x} e ${y} é ${resultado}`);
