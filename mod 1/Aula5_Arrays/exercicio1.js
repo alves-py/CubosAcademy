06ff0e943bb1b3394066c1a0140ae1c91bfba0a5
Alves-py <zashamktr@gmail.com>
2023-06-21 16:42:11 -0300

    git commit -m "Exercícios em aula - Arrays e loops - 21.06"

---
+++ mod 1/Aula5_Arrays/exercicio1.js
@@ -0,0 +1,16 @@
+const paises = ["Brasil", "EUA", "Japão","Chile", "Argentina"];
+
+paises.push("China");
+console.log(paises);
+paises.pop();
+console.log(paises);
+paises.unshift("Arabia");
+console.log(paises);
+paises.shift();
+console.log(paises);
+
+i = paises.length - 1;
+console.log(paises[i]);
+
+console.log(paises[1]);
+console.log(paises[2]);
