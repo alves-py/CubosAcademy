663ad8ed29823d3b690e0d4e02120170dc873622
Alves-py <zashamktr@gmail.com>
2023-06-28 11:48:42 -0300

    Exercicio 2

---
+++ Aula9_objetos/exercicio2.js
@@ -0,0 +1,24 @@
+const produtosConsumidos = [
+    {
+    nome: "Agua sanitária",
+    precoUnit: 1000,
+    quantidadeComprada: 2
+},
+{
+    nome: "Feijão",
+    precoUnit: 2000,
+    quantidadeComprada: 5
+},
+ {
+    nome: "arroz",
+    precoUnit: 1500,
+    quantidadeComprada: 10
+}];
+
+const cliente = {
+    nome: "Felipe",
+    idade: 25,
+    produtosConsumidos
+}
+
+console.log(cliente);
