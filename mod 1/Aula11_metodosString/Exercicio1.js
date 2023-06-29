557e9281802d2bb36470b577452f81e484e47e5c
Alves-py <zashamktr@gmail.com>
2023-06-29 19:56:19 -0300

    Exercicios em sala

---
+++ mod 1/Aula11_metodosString/Exercicio1.js
@@ -0,0 +1,5 @@
+const cidade = "Rio de Janeiro-RJ";
+
+const penultimoIndex = cidade.length - 2;
+let estado = cidade.slice(penultimoIndex);
+console.log(estado);
---
+++ mod 1/Aula11_metodosString/Exercicio2.js
@@ -0,0 +1,5 @@
+const numero = 97.50;
+const variavelAlterada = numero.replace(".",",");
+
+console.log(numero);
+console.log(variavelAlterada);
