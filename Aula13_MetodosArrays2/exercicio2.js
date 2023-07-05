2345ae5bb24c6e98db7032eefc34d9d2bafe1442
Alves-py <zashamktr@gmail.com>
2023-07-05 17:16:34 -0300

    Exercicios realizados em aula

---
+++ Aula13_MetodosArrays2/exercicio2.js
@@ -0,0 +1,21 @@
+const arquivos = [
+    'teste.txt',
+    'foto.png',
+    'contrato.doc',
+    'instalador.exe'
+];
+
+const antiVirus = (arrayArquivos) => {
+    const resultado = arrayArquivos.some((arquivo) => {
+        const existeExtensao = arquivo.indexOf('.bat');
+        return existeExtensao !== -1;
+    });
+
+    if (resultado) {
+        console.log('vírus detectado');
+    } else {
+        console.log('nenhum vírus encontrado');
+    }
+}
+
+antiVirus(arquivos);
